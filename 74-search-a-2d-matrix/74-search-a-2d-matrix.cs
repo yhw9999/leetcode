8bc828bf2f4e89c4e40f36b99cca7d2b4fd08868
yhw9999 <nyhw999@naver.com>
2022-03-30 11:39:39 +0900

    Time: 179 ms (6.87%), Space: 39.6 MB (30.71%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+	public bool SearchMatrix(int[][] matrix, int target)
+	{
+		var m = matrix.Length;
+		var n = matrix[0].Length;
+
+		for (int i = 0; i < m; i++)
+		{
+			if (target >= matrix[i].First() && matrix[i].Last() >= target)
+			{
+				var index = Array.BinarySearch(matrix[i], target);
+
+				if (index > -1)
+				{
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+}
