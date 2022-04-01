13f9d4cf286e052ecf87eac40f737e6078134f4f
yhw9999 <nyhw999@naver.com>
2022-04-01 18:00:56 +0900

    Time: 85 ms (83.92%), Space: 38.5 MB (31.28%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cs
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cs
@@ -20,22 +20,15 @@ public class Solution
 
 		var result = head;
 
-		while (true)
+		while (head.next != null)
 		{
-			if (head.next != null)
+			if (head.val == head.next.val)
 			{
-				if (head.val == head.next.val)
-				{
-					head.next = head.next.next;
-				}
-				else
-				{
-					head = head.next;
-				}
+				head.next = head.next.next;
 			}
 			else
 			{
-				break;
+				head = head.next;
 			}
 		}
 
