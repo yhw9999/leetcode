d6544886097e88e317ad0c447275f0d3f7074072
yhw9999 <nyhw999@naver.com>
2022-04-09 11:51:04 +0900

    Time: 182 ms (74.42%), Space: 44.7 MB (60.64%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cs
@@ -0,0 +1,20 @@
+public class Solution
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            if (!dict.TryGetValue(nums[i], out var value))
+            {
+                dict.Add(nums[i], 0);
+            }
+
+            dict[nums[i]]++;
+        }
+
+        var result = dict.OrderByDescending(x => x.Value).Select(x => x.Key).Take(k).ToArray();
+        return result;
+    }
+}
