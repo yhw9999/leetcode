4819652a2e46b1b7468c8e971865303675a08235
yhw9999 <nyhw999@naver.com>
2022-04-10 12:08:22 +0900

    Time: 101 ms (40.19%), Space: 35.3 MB (55.06%) - LeetHub

---
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cs
@@ -0,0 +1,24 @@
+public class Solution
+{
+    public string ConvertToTitle(int columnNumber)
+    {
+        var sb = new StringBuilder();
+        var stack = new Stack<char>();
+        var remain = 0;
+        while (columnNumber > 0)
+        {
+            remain = columnNumber % 26;
+            columnNumber--;
+            columnNumber /= 26;
+
+            stack.Push(remain == 0 ? 'Z' : (char)(remain + 64));
+        }
+
+        while (stack.Count > 0)
+        {
+            sb.Append(stack.Pop());
+        }
+
+        return sb.ToString();
+    }
+}
