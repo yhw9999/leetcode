e140e912e7c48c705046513a15a6b1175b6f66e9
yhw9999 <nyhw999@naver.com>
2022-04-10 21:21:39 +0900

    Time: 124 ms (99.58%), Space: 45.4 MB (76.67%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cs
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cs
@@ -4,9 +4,10 @@ public class Solution
     {
         var result = new int[nums.Length * 2];
 
-        for (int i = 0; i < nums.Length * 2; i++)
+        for (int i = 0; i < nums.Length; i++)
         {
-            result[i] = nums[i % nums.Length];
+            result[i] = nums[i];
+            result[i + nums.Length] = nums[i];
         }
 
         return result;
