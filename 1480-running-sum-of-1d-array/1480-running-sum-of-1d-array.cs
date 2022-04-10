31d6eedc8f808bff5bbf850809e5607727a1f57b
yhw9999 <nyhw999@naver.com>
2022-04-10 22:11:52 +0900

    Time: 184 ms (53.40%), Space: 41.8 MB (48.58%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
@@ -0,0 +1,12 @@
+public class Solution
+{
+    public int[] RunningSum(int[] nums)
+    {
+        for (int i = 1; i < nums.Length; i++)
+        {
+            nums[i] += nums[i - 1];
+        }
+
+        return nums;
+    }
+}
