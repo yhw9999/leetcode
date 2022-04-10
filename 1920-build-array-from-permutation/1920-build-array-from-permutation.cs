520698488f40b08a7859109e711c4230ed2ba31d
yhw9999 <nyhw999@naver.com>
2022-04-10 21:29:34 +0900

    Time: 211 ms (46.44%), Space: 45.4 MB (86.81%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cs
@@ -0,0 +1,14 @@
+public class Solution
+{
+    public int[] BuildArray(int[] nums)
+    {
+        var result = new int[nums.Length];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            result[i] = nums[nums[i]];
+        }
+
+        return result;
+    }
+}
