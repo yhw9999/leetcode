75b60744627f5fb2d2163c9a6f6bb89e76a67b60
yhw9999 <nyhw999@naver.com>
2022-04-15 23:46:46 +0900

    Time: 16 ms (98.04%), Space: 26.7 MB (16.34%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cs
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cs
@@ -2,14 +2,14 @@ public class Solution
 {
     public int MinBitFlips(int start, int goal)
     {
-        var result = start ^ goal;
+        start ^= goal;
 
-        result = (result & 0x55555555) + ((result >> 1) & 0x55555555);
-        result = (result & 0x33333333) + ((result >> 2) & 0x33333333);
-        result = (result & 0x0f0f0f0f) + ((result >> 4) & 0x0f0f0f0f);
-        result = (result & 0x00ff00ff) + ((result >> 8) & 0x00ff00ff);
-        result = (result & 0x0000ffff) + ((result >> 16) & 0x0000ffff);
+        start = (start & 0x55555555) + ((start >> 1) & 0x55555555);
+        start = (start & 0x33333333) + ((start >> 2) & 0x33333333);
+        start = (start & 0x0f0f0f0f) + ((start >> 4) & 0x0f0f0f0f);
+        start = (start & 0x00ff00ff) + ((start >> 8) & 0x00ff00ff);
+        start = (start & 0x0000ffff) + ((start >> 16) & 0x0000ffff);
 
-        return result;
+        return start;
     }
 }
