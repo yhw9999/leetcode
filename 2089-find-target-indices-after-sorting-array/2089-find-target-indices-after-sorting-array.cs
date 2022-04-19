f36c7c8b4959fef8d1f0af46f7aff7ddfe386d41
yhw9999 <nyhw999@naver.com>
2022-04-19 23:07:10 +0900

    Time: 296 ms (5.70%), Space: 42 MB (71.93%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cs
@@ -0,0 +1,23 @@
+public class Solution
+{
+    public IList<int> TargetIndices(int[] nums, int target)
+    {
+        var result = new List<int>();
+
+        Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                result.Add(i);
+            }
+            else if(nums[i] > target)
+            {
+                break;
+            }
+        }
+
+        return result;
+    }
+}
