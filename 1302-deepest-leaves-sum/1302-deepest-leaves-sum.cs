9d1aea5c0fcd6711183a5a63ce56ba2a797b7a32
yhw9999 <nyhw999@naver.com>
2022-05-15 22:23:17 +0900

    Time: 318 ms (5.08%), Space: 48 MB (18.64%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cs
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cs
@@ -11,59 +11,28 @@
  *     }
  * }
  */
-public class Solution
-{
-	public int DeepestLeavesSum(TreeNode root)
-	{
-		var deepestIndex = 0;
-
-		SetDeepestIndex(root, ref deepestIndex, 0);
-
-		var sum = 0;
-
-		SumDeepestValues(root, ref sum, 0, deepestIndex);
-
-		return sum;
-	}
-
-	private void SumDeepestValues(TreeNode node, ref int sum, int currentIndex, int deepestIndex)
-	{
-		if (node == null)
-		{
-			return;
-		}
-
-		if (currentIndex == deepestIndex)
-		{
-			sum += node.val;
-
-			return;
-		}
-
-		if (currentIndex < deepestIndex)
-		{
-			SumDeepestValues(node.left, ref sum, currentIndex + 1, deepestIndex);
-			SumDeepestValues(node.right, ref sum, currentIndex + 1, deepestIndex);
-		}
-
-		return;
-	}
-
-	private void SetDeepestIndex(TreeNode node, ref int deepestIndex, int currentIndex)
-	{
-		if (deepestIndex < currentIndex)
-		{
-			deepestIndex = currentIndex;
-		}
-
-		if (node.left != null)
-		{
-			SetDeepestIndex(node.left, ref deepestIndex, currentIndex + 1);
-		}
-
-		if (node.right != null)
-		{
-			SetDeepestIndex(node.right, ref deepestIndex, currentIndex + 1);
-		}
-	}
+public class Solution {
+    public int DeepestLeavesSum(TreeNode root) {
+        var dictionary = new Dictionary<int, List<int>>();
+        
+        AddValues(root, dictionary, 0);
+        
+        return dictionary.Last().Value.Sum();
+    }
+    
+    private void AddValues(TreeNode node, Dictionary<int, List<int>> dict, int currentIndex){
+        
+        if(node == null){
+            return;    
+        }
+        
+        if(!dict.TryGetValue(currentIndex, out var list)){
+            dict.Add(currentIndex, new List<int>());
+        }
+        
+        dict[currentIndex].Add(node.val);
+        
+        AddValues(node.left, dict, currentIndex + 1);
+        AddValues(node.right, dict, currentIndex + 1);
+    }
 }
