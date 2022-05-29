ce6ae0e527cc3c973ed51041475185e74aaef0f4
yhw9999 <nyhw999@naver.com>
2022-05-29 22:17:08 +0900

    Time: 2114 ms (5.02%), Space: 25.8 MB (40.26%) - LeetHub

--- 50-powx-n/50-powx-n.cs
+++ 50-powx-n/50-powx-n.cs
@@ -1,5 +1,44 @@
-public class Solution {
-    public double MyPow(double x, int n) {
-        return Math.Pow(x, n);
+public class Solution
+{
+    public double MyPow(double x, int n)
+    {
+        if(Math.Abs(x) == 1)
+        {
+            if(x < 0){
+                return n > 0 ? x : -x;
+            }
+            
+            return x;
+        }
+        
+        var result = x;
+
+        if (n > 0)
+        {
+            while (--n > 0)
+            {
+                result *= x;
+            }
+
+            return result;
+        }
+        else if (n < 0)
+        {
+            while (n++ <= 0)
+            {
+                if(result == 0)
+                {
+                    return result;
+                }
+                
+                result /= x;
+            }
+
+            return result;
+        }
+        else
+        {
+            return 1d;
+        }
     }
 }
