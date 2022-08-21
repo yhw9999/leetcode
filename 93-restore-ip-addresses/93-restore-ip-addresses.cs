1c18ab02f4d79866fb5fe85e55a2c2f4a20af741
yhw9999 <nyhw999@naver.com>
2022-08-21 18:58:52 +0900

    Time: 263 ms (11.43%), Space: 42.7 MB (50.71%) - LeetHub

---
+++ 93-restore-ip-addresses/93-restore-ip-addresses.cs
@@ -0,0 +1,67 @@
+public class Solution {
+    
+    List<string> result;
+    string literal;
+    
+    public IList<string> RestoreIpAddresses(string s) {
+        result = new List<string>();
+        literal = s;
+        
+        var stack = new Stack<byte>();
+        
+        Find(stack, 0);
+        
+        return result;
+    }
+    
+    void Find(Stack<byte> stack, int startIndex)
+    {
+        
+        if (stack.Count == 4)
+        {
+            if (literal.Length == startIndex)
+            {
+                result.Add(string.Join('.', stack.Reverse()));
+            }
+            
+            return;
+        }
+        
+        var endIndex = startIndex;
+        
+        while (GetAddressPart(startIndex, endIndex, out var part))
+        {
+            stack.Push(part);
+            Find(stack, endIndex + 1);
+            stack.Pop();
+            endIndex++;
+        }
+    }
+    
+    bool GetAddressPart(int startIndex, int endIndex, out byte part)
+    {
+        part = default;
+        
+        if (endIndex >= literal.Length)
+        {
+            return false;
+        }
+        
+        var partSize = endIndex - startIndex + 1;
+        
+        if (partSize > 1 && literal[startIndex] == '0')
+        {
+            return false;
+        }
+        
+        var subString = literal.Substring(startIndex, partSize);
+        
+        if (byte.TryParse(subString, out var toByte))
+        {
+            part = toByte;
+            return true;
+        }
+        
+        return false;
+    }
+}
