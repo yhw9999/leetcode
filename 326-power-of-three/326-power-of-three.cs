ac0d0c4824c2b5b3c4d8611f27b26897dd8c32a7
yhw9999 <nyhw999@naver.com>
2022-08-28 18:50:52 +0900

    Time: 116 ms (24.47%), Space: 29.3 MB (29.97%) - LeetHub

--- 326-power-of-three/326-power-of-three.cs
+++ 326-power-of-three/326-power-of-three.cs
@@ -1,17 +1,23 @@
 public class Solution {
     public bool IsPowerOfThree(int n) {
-        
-    if (n == 1)
-    {
-        return true;
-    }
-        
-    if ((n % 3 > 0 && n % 3 < 3) ||
-        n == 0)
+    
+    if(n == 0)
     {
         return false;
     }
+        
+    while(n > 2)
+    {
+        if(n % 3 == 0)
+        {
+            n /= 3;
+            continue;
+        }
+        else{
+            return false;
+        }
+    }
 
-    return IsPowerOfThree(n / 3);
+    return n == 1;
     }
 }
